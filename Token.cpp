796835770de56ce58e96340c7625a45c350f7ee5
Ian <ianhrdz4@gmail.com>
2026-05-10 16:07:01 -0700

    Fix token class

--- Token.cpp
+++ Token.cpp
@@ -1,9 +1,8 @@
 #include "Token.h"
 
-Token::Token(TokenType type, const std::string& lexeme,
-             const LiteralValue& literal, int line)
-    : type(type), lexeme(lexeme), literal(literal), line(line) {}
+Token::Token(TokenType type, const std::string& lexeme, int line)
+    : type(type), lexeme(lexeme), line(line) {}
 
 std::string Token::toString() const {
-    return std::to_string((int)type) + " " + lexeme;
+    return std::to_string((int) type) + " " + lexeme;
 }
--- Token.h
+++ Token.h
@@ -1,17 +1,14 @@
 #pragma once
 #include <string>
 #include "TokenType.h"
-#include "LiteralValue.h"
 
 class Token {
 public:
     TokenType type;
     std::string lexeme;
-    LiteralValue literal; 
     int line;
 
-    Token(TokenType type, const std::string& lexeme,
-          const LiteralValue& literal, int line);
+    Token(TokenType type, const std::string& lexeme, int line);
 
     std::string toString() const;
 };
